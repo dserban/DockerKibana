a078f7ab9fb9359b0c13627de8ae981e24d0a5c3
Dan Serban <dserban01@yahoo.com>
2017-03-18 19:31:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ echo 'Downloading JDK ...'
 wget --no-check-certificate --no-cookies                         \
      --header "Cookie: oraclelicense=accept-securebackup-cookie" \
      -qO /opt/jdk.tgz                                            \
-         http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.tar.gz
+         http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.tar.gz
 echo 'Extracting JDK ...'
 tar -xf /opt/jdk.tgz -C /opt
 rm /opt/jdk.tgz
